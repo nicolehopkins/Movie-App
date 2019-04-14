e32dc658a65f8f80750996bbf43d6498fe0aa8fd
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-04-14 16:37:46 -0400

    cleaning up old files + adding routes for App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './styling/NavBar.css'
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 
 // Components
@@ -10,9 +11,9 @@ import NavBar from './components/Navbar';
 class App extends Component {
   render() {
     return (
-      <>
-        <NavBar />
-      </>
+      <HashRouter>
+        <Route path='/' exact component={ NavBar} />
+      </HashRouter>
     );
   }
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,10 +4,10 @@ import React from 'react';
 const NavBar = (props) => {
 
   return(
-    <div>
-      <a href='/'>Home</a>
-      <a href='/movies'>All Movies</a>
-      <a href='/bygenre'>By Genre</a>
+    <div className='navbar'>
+      <a className='nav-links' href='/'>Home </a>
+      <a className='nav-links' href='/movies'>All Movies</a>
+      <a className='nav-links' href='/bygenre'>By Genre</a>
     </div>
   )
 }
