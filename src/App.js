3bfcc5be106045dbfc9f4f77e60b75ea645458e1
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-04-14 10:57:10 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Welcome to MovieApp</h1>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const NavBar = (props) => {
+
+  return(
+    <div>
+      <a href='/'>Home</a>
+      <a href='/movies'>All Movies</a>
+      <a href='/bygenre'>By Genre</a>
+    </div>
+  )
+}
+
+export default NavBar;
